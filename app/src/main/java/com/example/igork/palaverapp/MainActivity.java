b7621726fa843c573e792bdb6b898c07c14eec4d
palaverbest <palaverbest@gmail.com>
2017-06-07 16:17:30 +0200

    login

--- app/src/main/java/com/example/igork/palaverapp/MainActivity.java
+++ app/src/main/java/com/example/igork/palaverapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.igork.palaverapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button btn = (Button) findViewById(R.id.login_button_x);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
+
 }
