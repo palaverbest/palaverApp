956dd14f4e88e37cf081589d65649a6f6399c1a7
palaverbest <palaverbest@gmail.com>
2017-06-07 13:16:02 +0200

    Initial commit number 1 test

---
+++ app/src/main/java/com/example/igork/palaverapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.igork.palaverapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    private void TestUpload(){
+        int i = 5;
+        
+    }
+}
