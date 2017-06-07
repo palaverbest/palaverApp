49a315a2ec6b314f48a8d96436e601978abe49bf
palaverbest <palaverbest@gmail.com>
2017-06-07 14:24:01 +0200

    Registrierung Aktiviti

---
+++ app/src/main/java/com/example/igork/palaverapp/Registrierung.java
@@ -0,0 +1,13 @@
+package com.example.igork.palaverapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrierung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrierung);
+    }
+}
